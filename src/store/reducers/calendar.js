7ca2dd43deada53ab698b84f8fd8b20932a1be80
Arthur Mnyshenko <flushdevelopment@gmail.com>
2020-04-07 16:26:30 +0300

    change init events

--- src/store/reducers/calendar.js
+++ src/store/reducers/calendar.js
@@ -1,40 +1,36 @@
 const initialState = [
   {
     _id: "1",
-    title: "My custom event",
+    title: "test event #1",
     start: "2020-04-08T10:30:00Z",
     backgroundColor: "rgb(59, 134, 255)",
     editable: true,
-    extentedProps: {
-      status: "done",
-    },
-    description: "This is a cool eventsome note1@$51551",
+    description: "Note #1",
   },
   {
     _id: "2",
-    title: "event 334",
+    title: "test event #2",
     start: "2020-04-10",
     editable: true,
-    description:
-      "lond text description for width flex block testing lond text description for width flex block testing!@#!@$",
+    description: "Note #2",
     backgroundColor: "rgb(59, 134, 255)",
   },
   {
     _id: "3",
-    title: "1234567890123456789012345678901234567890",
+    title: "test event #3",
     start: "2020-04-21T14:40",
     end: "2020-04-22T19:40",
     editable: true,
-    description: "This is a cool eventsome note !!!",
+    description: "This is a cool awesome note",
     backgroundColor: "rgb(59, 134, 255)",
   },
   {
     _id: "4",
-    title: "123456789012345",
+    title: "Event with maximum chars432143",
     start: "2020-04-11",
     end: "2020-04-11",
     editable: true,
-    description: "lond text description for width flex b @!!",
+    description: "Test description",
     backgroundColor: "rgb(59, 134, 255)",
   },
 ];
